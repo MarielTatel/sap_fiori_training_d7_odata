6ef29ecc13a7e4ea83fa8dec83991825bd8602b4
Mariel Tatel <mariel.tatel@accenture.com>
2026-02-11 18:42:31 +0000

    Completed the activity requirement

--- odata/webapp/controller/Main.controller.js
+++ odata/webapp/controller/Main.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("sapips.training.odata.controller.Main", {
